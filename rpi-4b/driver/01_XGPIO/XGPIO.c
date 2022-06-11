0e15424004c2a74078cdc33073d641ed273c5794
xddcore <1034029664@qq.com>
2022-06-11 19:24:45 +0800

    upload xgpio

--- rpi-4b/driver/01_XGPIO/XGPIO.c
+++ rpi-4b/driver/01_XGPIO/XGPIO.c
@@ -2,7 +2,7 @@
  * @Author: Chengsen Dong 1034029664@qq.com
  * @Date: 2022-06-11 11:23:49
  * @LastEditors: xddcore 1034029664@qq.com
- * @LastEditTime: 2022-06-11 19:14:54
+ * @LastEditTime: 2022-06-11 19:24:17
  * @FilePath: /Embedded_Linux/rpi-4b/driver/01_XGPIO/XGPIO.c
  * @Description: XGPIO 树莓派4b BCM2711 GPIO Linux驱动
  */
@@ -91,7 +91,7 @@ static int __init XGPIO_Init(void)
     }
     printk(KERN_INFO "XGPIO: Register Device Success! Device Major = %d.\n", dev_major);
     //在虚拟内存中申请GPIO寄存器组的空间
-    if(!request_mem_region(XGPIO_Registerx_Base, sizeof(XGPIO_Registerx), XGPIO_Registerx_Name));
+    if(request_mem_region(XGPIO_Registerx_Base, sizeof(XGPIO_Registerx), XGPIO_Registerx_Name));
     {
         return -EINVAL;
     }
