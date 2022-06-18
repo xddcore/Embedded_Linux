6ad4ec85f4a90694d6f3b9ee1091466d96602ba9
Chengsen Dong <1034029664@qq.com>
2022-06-18 08:11:21 +0100

    Update XGPIO_APP.c

--- rpi-4b/driver/01_XGPIO/XGPIO_APP.c
+++ rpi-4b/driver/01_XGPIO/XGPIO_APP.c
@@ -2,7 +2,7 @@
  * @Author: Chengsen Dong 1034029664@qq.com
  * @Date: 2022-06-12 10:17:41
  * @LastEditors: Chengsen Dong 1034029664@qq.com
- * @LastEditTime: 2022-06-18 07:56:00
+ * @LastEditTime: 2022-06-18 08:11:14
  * @FilePath: /Embedded_Linux/rpi-4b/driver/01_XGPIO/XGPIO_APP.c
  * @Description: XGPIO 树莓派4b BCM2711 GPIO Linux驱动 的APP
  * 没用任何驱动框架，随便想着写的“野”驱动，
@@ -79,16 +79,16 @@ int main(int argc, char* argv[])
   char * cmd8="{gpio5|<setreset,false>}";//GPIO5设置为低电平
   printf("\n\nAPP send cmd to XGPIO Driver |by write|:%s\n",cmd1);
   write(fd,cmd1,strlen(cmd1));//GPIO2设置为输出模式
-  write(fd,cmd5,strlen(cmd5));//GPIO3，GPIO4设置为输入模式
-  write(fd,cmd6,strlen(cmd6));//GPIO5设置为输出模式
+  //write(fd,cmd5,strlen(cmd5));//GPIO3，GPIO4设置为输入模式
+  //write(fd,cmd6,strlen(cmd6));//GPIO5设置为输出模式
   for(i=0;i<10;i++)
   {
-    printf("APP send cmd to XGPIO Driver |by write|:%s\n",cmd4);
-    write(fd,cmd4,strlen(cmd4));//更新GPLEV0寄存器(可以读取GPIO0-31的电平状态)
-    printf("APP read GPLEV0 from XGPIO Driver |by read|\n");
-    read(fd,pGPLEV0,1);//读取一个1个基类型的长度
-    printf("GPLEV0 = 0x%x\n",GPLEV0);
-    
+    //printf("APP send cmd to XGPIO Driver |by write|:%s\n",cmd4);
+    //write(fd,cmd4,strlen(cmd4));//更新GPLEV0寄存器(可以读取GPIO0-31的电平状态)
+    //printf("APP read GPLEV0 from XGPIO Driver |by read|\n");
+    //read(fd,pGPLEV0,1);//读取一个1个基类型的长度
+    //printf("GPLEV0 = 0x%x\n",GPLEV0);
+
     printf("APP send cmd to XGPIO Driver |by write|:%s\n",cmd2);
     write(fd,cmd2,strlen(cmd2));
     //ioctl(fd, XGPIO_Registerx_Base+0x1c, 1<<(1*2));//设置为高电平
