21fa59fc4678ce6ad1e3b7a34e133c08f4d1cb90
Chengsen Dong <1034029664@qq.com>
2023-01-07 15:17:04 +0000

    Create XGPIO.c

---
+++ rpi-4b/driver/01_XGPIO/uio/XGPIO.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: Chengsen Dong 1034029664@qq.com
+ * @Date: 2023-01-07 15:16:28
+ * @LastEditors: Chengsen Dong 1034029664@qq.com
+ * @LastEditTime: 2023-01-07 15:16:52
+ * @FilePath: /Embedded_Linux/rpi-4b/driver/01_XGPIO/uio/XGPIO.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*XGPIO的uio实现*/
